d57c388e27fbef385e726287da79defdae371f3a
Steady-K <tkdlxm56@gmail.com>
2025-02-24 22:29:34 +0900

    Update 문자열 겹쳐쓰기.js

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.js
@@ -4,5 +4,4 @@ function solution(my_string, overwrite_string, s) {
     answer += overwrite_string;
     answer += my_string.slice(s + overwrite_string.length)
     return answer;
-    console.log(answer);
 }
