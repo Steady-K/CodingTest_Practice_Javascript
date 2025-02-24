d39a9aca8a2ad02c8f82328e97cda24abe94770d
Steady-K <tkdlxm56@gmail.com>
2025-02-24 22:20:40 +0900

    [level 0] Title: 문자열 섞기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.js
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.js
@@ -1,9 +1,3 @@
 function solution(str1, str2) {
-    var answer = '';
-    const Arr1 = [...str1];
-    const Arr2 = [...str2];
-    for (i = 0; i < str1.length; i++) {
-       answer += Arr1[i] + Arr2[i];
-    }
-    return answer;
+    return [...str1].map((x, idx) => x + str2[idx]).join('');
 }
