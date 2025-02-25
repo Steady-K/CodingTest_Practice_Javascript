e7bd581466b8a683a3aa90e1cd33f3d8e2170bd0
Steady-K <tkdlxm56@gmail.com>
2025-02-25 19:04:10 +0900

    [level 0] Title: 공배수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181936. 공배수/공배수.js
+++ 프로그래머스/0/181936. 공배수/공배수.js
@@ -1,9 +1,3 @@
 function solution(number, n, m) {
-    var answer = 0;
-    if (number % n === 0 && number % m === 0) {
-        answer = 1;
-    } else {
-        answer = 0;
-    }
-    return answer;
+    return +!(number % n || number % m);
 }
