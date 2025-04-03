fbfafd867cfd702e04e05a18678f84b30654620d
Steady-K <tkdlxm56@gmail.com>
2025-04-03 10:33:11 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -1,15 +1,9 @@
-function solution(x) {
-    let result = [];
-    while (true) {
-        result.push(x);
-        
-        if  (x === 1) break;
-        
-        if (x % 2 === 0) {
-            x = x / 2;
-        } else {
-            x = 3 * x + 1;
-        }
-    }
-    return result;
+function solution(n, arr = []) {
+    arr.push(n);
+    
+    if (n === 1) return arr;
+    
+    if (n % 2 === 0) return solution(n / 2, arr);
+    return solution(3 * n + 1, arr);
+    
 }
