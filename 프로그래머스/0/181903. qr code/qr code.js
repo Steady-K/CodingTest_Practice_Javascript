6d8e98ccc7ae569fa5c5a0285c7c8e199404ac91
Steady-K <tkdlxm56@gmail.com>
2025-04-21 20:10:14 +0900

    [level 0] Title: qr code, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181903. qr code/qr code.js
+++ 프로그래머스/0/181903. qr code/qr code.js
@@ -1,12 +1,3 @@
 function solution(q, r, code) {
-    const arr = [...code];
-    let result = ''
-    
-    for (let i = 0; i < code.length; i++) {
-        if (i % q === r) {
-            console.log(arr[i])
-            result += arr[i]
-        }
-    }
-    return result;
+    return [...code].filter((_, i) => i % q === r).join('');
 }
