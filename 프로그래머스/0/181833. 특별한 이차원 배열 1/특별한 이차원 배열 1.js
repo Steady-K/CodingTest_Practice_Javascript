f60ae5cb351aa15bbcc5070d7a7dec97024151b9
Steady-K <tkdlxm56@gmail.com>
2025-05-23 18:00:56 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.88 ms, Memory: 38 MB -BaekjoonHub

---
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
@@ -0,0 +1,12 @@
+function solution(n) {
+    const result = []
+    
+    for(let i = 0; i < n; i++) {
+        const row = []
+        for (let j = 0; j < n; j++) {
+            row.push(i === j ? 1 : 0);
+        }
+        result.push(row)
+    }
+    return result;
+}
