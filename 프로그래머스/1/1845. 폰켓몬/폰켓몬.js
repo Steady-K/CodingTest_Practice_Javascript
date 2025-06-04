cb105960e707a6e170a59a72368f2157784f5716
Steady-K <tkdlxm56@gmail.com>
2025-06-04 18:17:05 +0900

    [level 1] Title: 폰켓몬, Time: 0.55 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,10 +1,5 @@
 function solution(nums) {
     const set = new Set(nums);
     const max = nums.length / 2;
-    console.log(set)
-    if(set.size > max) {
-        return max;
-    } else if (set.size < max) {
-        return set.size;
-    } return set.size;
+    return Math.min(set.size, max);
 }
