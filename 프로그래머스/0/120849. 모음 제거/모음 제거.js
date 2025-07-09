27d4fd105272a2cd75e8941ce499e1273b1cbdac
Steady-K <tkdlxm56@gmail.com>
2025-07-09 13:35:35 +0900

    [level 0] Title: 모음 제거, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.js
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.js
@@ -1,3 +1,4 @@
 function solution(my_string) {
-    return my_string.replace(/[aeiou]/g, '')
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    return [...my_string].filter(ch => !vowels.includes(ch)).join('')
 }
