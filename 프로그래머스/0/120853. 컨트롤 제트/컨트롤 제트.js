a37aac72bab952852a58196b492d5ed0d8b7a5a8
Steady-K <tkdlxm56@gmail.com>
2025-07-10 12:50:16 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.18 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.js
@@ -0,0 +1,13 @@
+function solution(s) {
+    const tokens = s.split(' ');
+    const stack = [];
+    
+    for (let token of tokens) {
+        if (token === 'Z') {
+            stack.pop()
+        } else {
+            stack.push(Number(token))
+        }
+    }
+    return stack.reduce((acc, cur) => acc + cur, 0);
+}
