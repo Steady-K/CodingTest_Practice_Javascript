392d8871e5accc870f918b65707c7244a0700b28
Steady-K <tkdlxm56@gmail.com>
2025-08-02 16:22:17 +0900

    [level 0] Title: 이진수 더하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.js
@@ -0,0 +1,9 @@
+function solution(bin1, bin2) {
+    const numA = parseInt(bin1, 2);
+    const numB = parseInt(bin2, 2);
+    
+    const sum = numA + numB;
+    return sum.toString(2)
+    
+    
+}
