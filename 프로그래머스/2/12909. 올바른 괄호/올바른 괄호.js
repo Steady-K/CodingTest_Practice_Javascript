08b0f694699e6f3dbc94ec5d85193da233544b55
Steady-K <tkdlxm56@gmail.com>
2025-08-08 20:50:27 +0900

    [level 2] Title: 올바른 괄호, Time: 29.86 ms, Memory: 37.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,13 @@
+function solution(s){
+    let count = 0;
+    
+    for(let char of s) {
+        if(char === '(') count++;
+        else count--;
+        
+        if(count < 0) return false;
+    }
+    return count === 0 ? true : false;
+
+    
+}
