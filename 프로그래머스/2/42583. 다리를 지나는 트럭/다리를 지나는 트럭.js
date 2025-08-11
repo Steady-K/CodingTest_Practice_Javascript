6aa1c72788d23688ad343437220638d5ec10a85a
Steady-K <tkdlxm56@gmail.com>
2025-08-11 16:39:55 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 28.32 ms, Memory: 37.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -0,0 +1,22 @@
+function solution(bridge_length, weight, truck_weights) {
+    const bridge = Array(bridge_length).fill(0);
+    
+    let time = 0;
+    let currentWeight = 0;
+    let i = 0;
+    
+    while(i < truck_weights.length || currentWeight > 0) {
+        time++
+        
+        currentWeight -= bridge.shift();
+        
+        if(i < truck_weights.length && currentWeight + truck_weights[i] <= weight) {
+            bridge.push(truck_weights[i]);
+            currentWeight += truck_weights[i];
+            i++
+        } else {
+            bridge.push(0);
+        }
+    }
+    return time;
+}
