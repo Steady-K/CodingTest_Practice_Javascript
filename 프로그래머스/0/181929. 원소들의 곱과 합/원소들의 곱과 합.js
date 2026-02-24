850d4e6ce5cbbe8329b974c317c0e39fdc4af570
Steady-K <tkdlxm56@gmail.com>
2026-02-24 17:21:04 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.js
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.js
@@ -1,16 +1,11 @@
 function solution(num_list) {
-    let times = 1;
+    const len = num_list.length;
+    let product = 1;
     let sum = 0;
     
-    for (let i = 0; i < num_list.length; i++) {
-        times *= num_list[i];
+    for(let i = 0; i < len; i++) {
+        product *= num_list[i]
         sum += num_list[i]
     }
-    if (times < sum**2) {
-        return 1;
-    } else {
-        if (times > sum**2) {
-            return 0;
-        }
-    }
+    return product < sum*sum ? 1 : 0  
 }
