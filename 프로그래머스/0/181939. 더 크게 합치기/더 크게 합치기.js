6c5470daaddf6422821a29bee1b3e9d919992026
Steady-K <tkdlxm56@gmail.com>
2026-02-24 16:38:09 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
@@ -1,9 +1,3 @@
 function solution(a, b) {
-    var answer = 0;
-    if (Number(a.toString() + b.toString()) >= Number(b.toString() + a.toString())) {
-        answer = a.toString() + b.toString()
-    } else {
-        answer = b.toString() + a.toString()
-    }
-    return Number(answer);
+    return Math.max(Number(`${a}` + `${b}`), Number(`${b}` + `${a}` ))
 }
