e453402f062728b2e1a4e33a83d539889b60a5f7
Steady-K <tkdlxm56@gmail.com>
2026-02-24 16:29:57 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
@@ -1,16 +1,13 @@
 function solution(n) {
-    let odd = 0;
-    let even = 0;
+    let sum = 0
     if(n % 2 !== 0) {
         for(let i = 1; i <= n; i+=2) {
-            odd += i
+            sum += i
         } 
-        return odd
     } else {
         for(let i = 2; i <= n; i+=2) {
-            even += i*i
+            sum += i*i
         }
-        return even
     }
-    
+    return sum
 }
