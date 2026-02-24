7b8938c1feb695f69c916e8157cfab565532d9d5
Steady-K <tkdlxm56@gmail.com>
2026-02-24 16:51:12 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
@@ -1,17 +1,10 @@
 function solution(num_list) {
-    
-    let last = num_list[num_list.length - 1];
-    let secondLast = num_list[num_list.length - 2];
-    
-    let result1 = last - secondLast;
-    let result2 = last*2
-    
-    if (last > secondLast) {
-         num_list.push(result1);
-    } else {
-         num_list.push(result2);
-    }
-     return num_list;
+    const numListLen = num_list.length;
+    const lastNum = num_list[numListLen - 1]
+    const lastPrevNum = num_list[numListLen - 2]
+
+    const addNum =  lastNum > lastPrevNum ? lastNum - lastPrevNum : lastNum * 2
+
+    return [...num_list, addNum]
+
 }
-   
-    
