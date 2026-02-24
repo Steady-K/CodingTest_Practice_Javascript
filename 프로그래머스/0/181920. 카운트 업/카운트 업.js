70d4548cb80cbe376ac72db2df8e4db90c5ace3f
Steady-K <tkdlxm56@gmail.com>
2026-02-24 16:31:59 +0900

    [level 0] Title: 카운트 업, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.js
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.js
@@ -1,8 +1,7 @@
 function solution(start_num, end_num) {
-    let result = [];
-    
-    for (let i = start_num; i <= end_num; i ++) {
-        result.push(i)
+    let answer = []
+    for(let i = start_num; i <= end_num; i++) {
+        answer.push(i)
     }
-    return result;
+    return answer
 }
