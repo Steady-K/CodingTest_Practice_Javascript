aee11039de50daf5a6ed28efb47a3b6844e5c936
Steady-K <tkdlxm56@gmail.com>
2026-03-17 17:54:28 +0900

    [level 1] Title: 추억 점수, Time: 2.55 ms, Memory: 35.9 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.js
@@ -1,14 +1,3 @@
 function solution(name, yearning, photo) {
-    let result = []
-    let sum = 0
-    for(let i = 0; i < photo.length; i++) {
-        for(let j = 0; j < name.length; j++) {
-            if(photo[i].includes(name[j])){
-                sum += Number(yearning[j])
-            }
-        }
-        result.push(sum)
-        sum = 0
-    }
-    return result;
+    return photo.map((v) => v.reduce((a, c) => a += yearning[name.indexOf(c)] ?? 0, 0))
 }
