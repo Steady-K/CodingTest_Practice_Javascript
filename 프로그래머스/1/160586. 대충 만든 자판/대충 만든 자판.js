2f45007606e67ab0983ee1052da1ec88fa929b94
Steady-K <tkdlxm56@gmail.com>
2026-03-19 11:53:17 +0900

    [level 1] Title: 대충 만든 자판, Time: 9.04 ms, Memory: 35.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.js
@@ -0,0 +1,28 @@
+function solution(keymap, targets) {
+    const result = []
+    
+    targets.forEach((target) => {
+        let totalPush = 0;
+        let possible = true;
+        
+        for (let i = 0; i < target.length; i++) {
+            const char = target[i]
+            let minPush = 101
+            
+            keymap.forEach((key) => {
+                const idx = key.indexOf(char)
+                if(idx !== -1) {
+                    minPush = Math.min(minPush, idx + 1)
+                }
+            })
+            
+            if(minPush === 101) {
+                possible = false
+                break
+            }
+            totalPush += minPush
+        }
+        result.push(possible ? totalPush : -1)
+    })
+    return result
+}
