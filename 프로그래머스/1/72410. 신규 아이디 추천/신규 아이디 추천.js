431e9eb2b7ecfd554d5fa3819ad8d1a6394e73c3
Steady-K <tkdlxm56@gmail.com>
2026-03-25 14:12:37 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.35 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -0,0 +1,18 @@
+function solution(new_id) {
+    
+    let id = new_id
+        .toLowerCase()
+        .replace(/[^a-z0-9\-_.]/g, "")
+        .replace(/\.{2,}/g, ".")
+        .replace(/^\.+|\.+$/g, "")
+        || "a"
+    
+    if(id.length >= 16) {
+        id = id.slice(0, 15).replace(/\.$/, "")
+    }
+    
+    while(id.length < 3) {
+        id += id[id.length - 1]
+    }
+    return id
+}
