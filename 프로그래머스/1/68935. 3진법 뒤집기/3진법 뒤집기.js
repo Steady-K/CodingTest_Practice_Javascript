c668963f32afd78871b2479c9bc42f6ea28aae98
Steady-K <tkdlxm56@gmail.com>
2026-03-25 13:16:03 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -1,14 +1,3 @@
 function solution(n) {
-    const ternary = []
-    while(n > 0) {
-        ternary.unshift(n % 3)
-        n = Math.floor(n / 3)
-    }
-    ternary.reverse()
-
-    let result = 0
-    for(let i = 0; i < ternary.length; i++) {
-        result += ternary[i] * (3 ** (ternary.length - 1 - i))
-    }
-    return result
+    return parseInt([...n.toString(3)].reverse().join(''), 3)
 }
