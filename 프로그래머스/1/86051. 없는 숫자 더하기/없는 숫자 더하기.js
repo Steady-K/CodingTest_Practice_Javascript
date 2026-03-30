3ca27acb40df31a036027c1c526382c2b5b52a83
Steady-K <tkdlxm56@gmail.com>
2026-03-30 18:53:07 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,11 +1,3 @@
 function solution(numbers) {
-    let s = "0123456789"
-    let result = 0
-    
-    for(let i = 0; i < numbers.length; i++) {
-        if(s.includes(numbers[i])) {
-           s = s.replace(String(numbers[i]), "")
-        }
-    }
-    return [...s].map(Number).reduce((sum, cur) => sum + cur, 0)
+    return 45 - numbers.reduce((acc, cur) => acc + cur, 0)
 }
